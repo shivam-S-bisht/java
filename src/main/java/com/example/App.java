8ce8e5fd6934750879b73c369a64a96b9fd8a4d9
Shivam Bisht <bishtshivam096@gmail.com>
2021-06-11 23:01:31 +0530

    Threading/ Runnable

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,96 +1,46 @@
 package com.example;
 
-import java.io.*;
-import java.util.*;
-
 class App {
 
-    private static class Node {
-        int data;
-        ArrayList<Node> children = new ArrayList<>();
-    }
-
-    public static void display(Node node) {
-
-        String str = node.data + "->";
-        for (Node child : node.children) {
-            str += child.data + ", ";
+    static class Parent1 extends Thread {
+        void show() {
+            for (int i=0; i<10; i++)
+            System.out.println("Parent 1");
         }
-        str += ".";
-        System.out.println(str);
 
-        for (Node child : node.children) {
-            display(child);
+        public void run() {
+            show();
         }
     }
 
-    public static Node construct(int[] arr) {
-        Node root = null;
+    static class Parent2 extends Thread {
+        void show() {
+            for (int i=0; i<10; i++)
 
-        Stack<Node> st = new Stack<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == -1) {
-                st.pop();
-            } else {
-                Node t = new Node();
-                t.data = arr[i];
-
-                if (st.size() > 0) {
-                    st.peek().children.add(t);
-                } else {
-                    root = t;
-                }
-                st.push(t);
-            }
+            System.out.println("Parent 2");
         }
-        return root;
+
+        public void run() {
+            show();
+        }
     }
 
-    public static boolean find(Node node, int data) {
-        if (node.data == data) {
-            return true;
-        }
+    public void hello () {
 
-        for (Node child : node.children) {
-            if (find(child, data)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static ArrayList<Integer> nodeToRootPath (Node node, int data) {
-        if (node.data == data) {
-            ArrayList<Integer> bres = new ArrayList<>();
-            bres.add(data);
-            return bres;
-        }
-
-        for (Node child: node.children) {
-            ArrayList<Integer> mres = nodeToRootPath(child, data);
-            if (mres.size()!=0) {
-                mres.add(node.data);
-                return mres;
-            }
-        }
-
-        ArrayList<Integer> mres = new ArrayList<>();
-        return mres;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int data = sc.nextInt();
-        sc.close();
+
+        Parent1 p1 = new Parent1();
+        Parent2 p2 = new Parent2();
+
+        // Thread t1 = new Thread(p1);
+        // Thread t2 = new Thread(p2);
+
+        p1.start();
+        p2.start();
+
         
-        Node root = construct(arr);
-        System.out.println(find(root, data));
-        System.out.println(nodeToRootPath(root, data));
 
     }
 }
