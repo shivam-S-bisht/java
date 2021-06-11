8b52cc56455e10f1d85799dfb144e4757fdbf7e9
Shivam Bisht <bishtshivam096@gmail.com>
2021-06-11 23:15:48 +0530

    new runnable insidde thread

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,26 +8,23 @@ class App {
 
     public static void main(String[] args) {
 
-        Runnable r1 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("Parent 1");
-                }
-            }
-        };
-
-        Runnable r2 = new Runnable() {
+        Thread t1 = new Thread(new Runnable() {
 
             public void run () {
                 for (int i=0; i<10; i++) {
-
+                    System.out.println("Parent 1");
                 }
             }
-        };
+        });
 
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
+        Thread t2 = new Thread(new Runnable() {
+
+            public void run () {
+                for (int i=0; i<10; i++) {
+                    System.out.println("Parent 2");
+                }
+            }
+        });
 
         t1.start();
         t2.start();
