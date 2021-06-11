217494fe1c41df8a5a208e3fc0d73042a03a1a50
Shivam Bisht <bishtshivam096@gmail.com>
2021-06-11 23:37:47 +0530

    threads with shared mems

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,32 +2,36 @@ package com.example;
 
 class App {
 
-    public void hello() {
+    static int count = 0;
 
+    static synchronized void incr () {
+        ++count;
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) throws Exception{
 
-        Thread t1 = new Thread(new Runnable() {
 
+        Thread t1 = new Thread (new Runnable(){
             public void run () {
-                for (int i=0; i<10; i++) {
-                    System.out.println("Parent 1");
-                }
+                for (int i=0; i<10000; i++)
+                incr();
             }
         });
 
-        Thread t2 = new Thread(new Runnable() {
 
-            public void run () {
-                for (int i=0; i<10; i++) {
-                    System.out.println("Parent 2");
-                }
-            }
+        Thread t2 = new Thread (new Runnable(){
+           public void run () {
+               for (int i=0; i<10000; i++)
+               incr();
+           } 
         });
 
         t1.start();
         t2.start();
 
+        // incr();
+        t1.join();
+        t2.join();
+        System.out.println(count);
     }
 }
