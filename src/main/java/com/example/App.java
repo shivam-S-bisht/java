f3f0bb4ed7b234317601884edc61f00954dbf28a
Shivam Bisht <bishtshivam096@gmail.com>
2021-06-13 19:16:13 +0530

    constructing and displaying the tree from an array

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,37 +1,59 @@
 package com.example;
 
+import java.security.spec.DSAGenParameterSpec;
+import java.util.*;
+
 class App {
 
-    static int count = 0;
-
-    static synchronized void incr () {
-        ++count;
+    static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
     }
 
-    public static void main (String[] args) throws Exception{
-
-
-        Thread t1 = new Thread (new Runnable(){
-            public void run () {
-                for (int i=0; i<10000; i++)
-                incr();
+    //
+    public static Node construct(int[] arr, int size) {
+        Queue<Node> q = new ArrayDeque<>();
+        Queue<Node> h = new ArrayDeque<>();
+        Node root = null;
+        for (int i = 0; i < arr.length; i++) {
+            Node node = new Node();
+            node.data = arr[i];
+            if (q.size() == 0) {
+                root = node;
+                q.add(root);
+            } else {
+                if (q.peek().children.size() == 3) {
+                    q.remove();
+                    if (q.size() == 0) {
+                        q = h;
+                        h = new ArrayDeque<>();
+                    }
+                }
+                q.peek().children.add(node);
+                h.add(node);
             }
-        });
+        }
 
+        return root;
 
-        Thread t2 = new Thread (new Runnable(){
-           public void run () {
-               for (int i=0; i<10000; i++)
-               incr();
-           } 
-        });
+    }
 
-        t1.start();
-        t2.start();
+    public static void display(Node node) {
+        String str = node.data + "->";
+        for (Node child : node.children) {
+            str += child.data + ",";
+        }
+        str += ".";
+        System.out.println(str);
 
-        // incr();
-        t1.join();
-        t2.join();
-        System.out.println(count);
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        display(construct(arr, 3));
+
     }
 }
